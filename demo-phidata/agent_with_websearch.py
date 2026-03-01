202c51dc1a340e7950301aaf7b03161c85d41247
admin <praveen.trainer@outlook.com>
2026-03-01 15:53:46 +0000

    use newspaper4k

--- demo-phidata/agent_with_websearch.py
+++ demo-phidata/agent_with_websearch.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.newspaper4k import Newspaper4k
 
 from dotenv import load_dotenv
 
@@ -24,14 +25,15 @@ def create_websearch_agent():
             "Include sources in your responses.",
             "Summarize findings clearly.",
             "Use the get_current_datetime tool to get today's date and time when the query involves current events, recent news, or time-sensitive information.",
+            "Use the Newspaper4k tool to scrape and read the full content of a URL when deeper article details are needed beyond search snippets.",
         ],
         show_tool_calls=True,
         markdown=True,
         debug_mode=False, 
-        tools=[DuckDuckGo(), get_current_datetime]
+        tools=[DuckDuckGo(), Newspaper4k(), get_current_datetime]
     )
     return agent
 
 if __name__ == "__main__":
     agent = create_websearch_agent()
-    agent.print_response("what are the new movies release in hollywood", stream=True)
+    agent.print_response("what are the new movies release in hollywood and also some details about these movies", stream=True)
